10ffcd36169d55126bf0f6fa4b14d37f64852a2f
Jordan Endicott <jordan.endicott823@gmail.com>
2018-06-13 15:06:56 -0400

    added pizza program

---
+++ pizza_app.rb
@@ -0,0 +1,52 @@
+def meats ()
+    meats = ["pepperoni", "sausage" ,"ham", "bacon"]
+end
+
+def crust ()
+    crust = ["normal", "thin", "stuffed", "deep dish"]
+end
+
+def sauce()
+    sauces = ["marinara", "spicy", "ranch"]
+end
+
+def veggies()
+    veggies = ["onions", "green peppers", "bananna peppers"]
+end
+
+def cheese()
+    cheese = ["mozzerella", "cheddar", "pepper jack"]
+end
+
+def pizza_maker
+    puts "How many pizzas would you like?"
+    
+    pizza_counter = gets.chomp.to_i
+        while pizza_counter > 0 do
+        puts "You are getting a #{meats.sample}, #{crust.sample} crust pizza, with #{veggies.sample}, #{sauce.sample} sauce, and #{cheese.sample} cheese..."
+        pizza_counter -= 1
+    end
+
+    loop do
+        puts "Is that good? yes or no?"
+        answer = gets.chomp
+            if answer == "yes"
+                exit
+            elsif answer == "no"
+                pizza_maker
+            elsif answer == "pizza pie"
+                puts "PEPPERONI AND GREEN PEPPERS MUSHROOMS OLIVES CHIVES"
+                sleep 0.3
+                puts "pepperoni and green peppers mushrooms olives chives"
+                sleep 1
+                puts "NEED THERAPY, THERAPY...ADVERTISING CAUSES NEED THERAPY, THERAPY!"
+                sleep 1
+                pizza_maker
+            else 
+        puts "please type yes or no"
+
+
+        end
+    end
+end
+pizza_maker
