a2ddb4b30360f0260ef00d5dc086dcde197fd57e
Jordan Endicott <jordan.endicott823@gmail.com>
2018-06-15 13:34:09 -0400

    set up a way for people to select sizes, added prices

--- pizza_app.rb
+++ pizza_app.rb
@@ -1,44 +1,77 @@
+def size()
+    size = ["small", "medium", "large", "extra large"]
+    size[0..4]
+end
+
 def meats ()
     meats = ["pepperoni", "sausage" ,"ham", "bacon"]
+    meats[0..4]
 end
 
 def crust ()
     crust = ["normal", "thin", "stuffed", "deep dish"]
+    crust[0..4]
 end
 
 def sauce()
     sauces = ["marinara", "spicy", "ranch"]
+    sauces[0..3]
 end
 
 def veggies()
     veggies = ["onions", "green peppers", "bananna peppers"]
+    veggies[0..3]
 end
 
 def cheese()
     cheese = ["mozzerella", "cheddar", "pepper jack"]
+    cheese[0..3]
 end
 
-def pizza_maker
-    puts "How many pizzas would you like?"
-    
-    pizza_counter = gets.chomp.to_i
-        while pizza_counter > 0 do
-        puts "You are getting a #{meats.sample}, #{crust.sample} crust pizza, with #{veggies.sample}, #{sauce.sample} sauce, and #{cheese.sample} cheese..."
-        pizza_counter -= 1
+def pizza_maker(size, meats, crust, sauce, veggies, cheese)
+    puts "Enter the corresponding number to make your selections"
+    puts "please select size" ;print "#{size()} : "; pizza_size = $stdin.gets.chomp
+    puts "please select meats" ;print "#{meats()} :" ;pizza_meat = $stdin.gets.chomp
+    puts "please select crust" ;print "#{crust()} :" ;pizza_crust = $stdin.gets.chomp
+    puts "please select sauce" ;print "#{sauce()} :" ;pizza_sauce = $stdin.gets.chomp
+    puts "please select any additional topping" ;print "#{veggies()} :";pizza_veg= $stdin.gets.chomp
+    puts "please select cheese" ;print "#{cheese()} :" ;pizza_cheese = $stdin.gets.chomp
+    price = 0
+    case pizza_size.to_i
+    when 2; pizza_size = size[0]; price += 2
+    when 3; pizza_size = size[1]; price += 5
+    when 4; pizza_size = size[2]; price += 10
     end
-
-    loop do
-        puts "Is that good? yes or no?"
-        answer = gets.chomp
-            if answer == "yes"
-                exit
-            elsif answer == "no"
-                pizza_maker
-            else 
-        puts "please type yes or no"
-
-
-        end
+    case pizza_meat.to_i
+    when 1; pizza_meat = meats[0]; price += 2
+    when 2; pizza_meat = meats[1]; price += 2
+    when 3; pizza_meat = meats[2]; price += 2
+    when 4; pizza_meat = meats[3]; price += 2
     end
+    case pizza_crust.to_i
+    when 1; pizza_crust = crust[0]; price += 0
+    when 2; pizza_crust = crust[1]; price += 1
+    when 3; pizza_crust = crust[2]; price += 3
+    when 4; pizza_crust = crust[3]; price += 5
+    end
+    case pizza_sauce.to_i
+    when 1; pizza_sauce = sauce[0]; price += 1
+    when 2; pizza_sauce = sauce[1]; price += 1
+    when 3; pizza_sauce = sauce[2]; price += 1
+    end 
+    case pizza_veg.to_i
+    when 1; pizza_veg = veggies[0]; price += 1
+    when 2; pizza_veg = veggies[1]; price += 1
+    when 3; pizza_veg = veggies[2]; price += 1
+    end
+    case pizza_cheese.to_i
+    when 1; pizza_cheese = cheese[0]; price += 1
+    when 2; pizza_cheese = cheese[1]; price += 1
+    when 3; pizza_cheese = cheese[2]; price += 2
+    end
+    total = price
+    puts "okay, that'll be a #{pizza_size} pizza, on #{pizza_crust} crust, with #{pizza_sauce} sauce, toppings include #{pizza_meat} #{pizza_veg} and #{pizza_cheese} cheese. "
+    puts " that'll come to #{total}"
 end
-pizza_maker
+ pizza_maker(size, meats, crust, sauce, veggies, cheese)
+
